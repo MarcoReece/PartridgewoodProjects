3a774f1bb81cc19a9ca864434174e9dcfd5d4dd6
Ross Nolan <marco.dumbleton@fostermelliar.co.za>
2023-10-31 14:41:48 +0200

    services auto scroll cleanup

--- src/app/servicess/services.component.ts
+++ src/app/servicess/services.component.ts
@@ -22,84 +22,47 @@ export class ServicesComponent implements OnInit {
   scrollToPainting() {
     const element = this.paintingSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
     element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
   scrollToDamp() {
     const element = this.dampSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
-    element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
   scrollToBuilding() {
     const element = this.buildingSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
     element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
   scrollToGutters() {
     const element = this.guttersSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
     element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
   scrollToTiling() {
     const element = this.tilingSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
     element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
   scrollToDoor() {
     const element = this.doorSection.nativeElement;
     const elementPosition = element.getBoundingClientRect();
-    
-    // Calculate the scroll position to center the element in the viewport
     const offset = elementPosition.top - (window.innerHeight - elementPosition.height) / 2;
-    
-    // Use scrollIntoView with behavior 'smooth' to scroll to the element
     element.scrollIntoView({ behavior: 'smooth' });
-    
-    // Adjust the scroll position to make it more precise
     window.scrollBy(0, offset);
   }
 
